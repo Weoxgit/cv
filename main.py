604b2cc94e341ec20bcfb8cabd69704c87222222
Weoxgit <95112478+Weoxgit@users.noreply.github.com>
2021-11-29 18:00:21 +0600

    Add initial CV website files

---
+++ main.py
@@ -0,0 +1,16 @@
+# html - structure of the website (create buttons etc.)
+# css - styling of the house
+# js - allows website to have behavior
+# heading tags require closing and starting
+# br does not require closing
+
+# opening tag -> <h2>Arthur Conan Doyle</h2> <- closing tag
+# <br> - self-closing tag
+# <hr <- html elemtent, html attribute -> size="3"> => <hr size="3">
+
+# <!-- X -->    <-comment in html
+# utf-8 is the most standart + includes emojis
+# <em> for italic and <strong> for bold
+# <ul> - un ordered list
+# <img> - to paste images, alt - provides with text if image is not available, also helps with SEO
+# deprecated - not recommended to use
