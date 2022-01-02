905127186a6b99372186066bebb14093081a8f09
Weoxgit <95112478+Weoxgit@users.noreply.github.com>
2022-01-02 11:58:52 +0600

    index.html update

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# html - structure of the website (create buttons etc.)
-# css - styling of the house
-# js - allows website to have behavior
-# heading tags require closing and starting
-# br does not require closing
+from flask import Flask, render_template
+import time
 
-# opening tag -> <h2>Arthur Conan Doyle</h2> <- closing tag
-# <br> - self-closing tag
-# <hr <- html elemtent, html attribute -> size="3"> => <hr size="3">
+app = Flask(__name__)
 
-# <!-- X -->    <-comment in html
-# utf-8 is the most standart + includes emojis
-# <em> for italic and <strong> for bold
-# <ul> - un ordered list
-# <img> - to paste images, alt - provides with text if image is not available, also helps with SEO
-# deprecated - not recommended to use
+
+@app.route("/")
+def home():
+    current_year = time.strftime("%Y")
+    return render_template("index.html", year=current_year)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
